c61ffdfe12a718aa30a4b66658a7c5b14ced4ad9
Mapletestm <mapleworld12@gmail.com>
2024-09-16 11:22:20 -0400

    Delete logofftest.bat

--- logofftest.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -command "[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms') | Out-Null; [System.Windows.Forms.MessageBox]::Show('Your computer has been hacked. Goodbye!', 'Security Alert', 'OK', 'Warning')"
